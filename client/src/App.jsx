35294d68bfd78e9de81c79bc207398de81ec011f
fatma moataz <fatmamoataz65@gmail.com>
2026-02-23 22:28:29 +0200

    background color Login updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -44,7 +44,7 @@ return <Loading/>
         </div>
       </div>
 ) : (
-<div className="bg-gradient-to-b from-[#242124] to-[#000000] flex items-center justify-center h-screen w-screen">
+<div className="bg-gradient-to-b from-[#531B81] to-[#d11382] flex items-center justify-center h-screen w-screen">
   <Login/>
 </div>
 )}
